eb32dbae44da5c7d5434f1b107c7e50e55356fb8
yanglulu <yanglu_u@126.com>
2021-06-24 19:22:37 +0800

    commit by shell

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@
 hexo clean
 hexo g
 hexo d
-echo -e "\033[32m deploy to dbses.github.io SUCCESS. \033[0m"
+echo -e "\033[32m [1] deploy to dbses.github.io SUCCESS. \033[0m"
 
 git add .
 git commit -m 'commit by shell'
 git push
-echo -e "\033[32m push to myblog SUCCESS. \033[0m"
+echo -e "\033[32m [2] push to myblog SUCCESS. \033[0m"
