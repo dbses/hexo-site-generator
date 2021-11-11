8d3197474c66c00a1eff75307d821db23216a9e2
yanglulu <lulu.yang@casstime.com>
2021-11-11 16:48:07 +0800

    commit by shell

---
+++ commit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git add .
+git commit -m 'commit by shell'
+git push
+echo -e "\033[32m [2] push to hexo-site-generator SUCCESS. \033[0m"
+
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# infra blog dir
+# generate website
 hexo clean
 hexo g
-hexo d
-echo -e "\033[32m [1] deploy to dbses.github.io SUCCESS. \033[0m"
 
+# domain file for github
+cp CNAME public
+
+cd public
+git init
 git add .
-git commit -m 'commit by shell'
-git push
-echo -e "\033[32m [2] push to myblog SUCCESS. \033[0m"
+git commit -m 'deploy by shell'
+# git push -f {your project} master:{your project branch}
+git push -f git@github.com:dbses/dbses.github.io.git master:webstack
+echo -e "\033[32m [1] deploy SUCCESS. \033[0m"
+
+cd -
